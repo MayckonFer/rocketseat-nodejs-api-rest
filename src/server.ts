2d57ed16aa62422ee5a23617f623f11c4aca1f9a
MayckonFer <mayckongamer15@gmail.com>
2025-06-07 12:02:33 -0300

    finish api

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,5 @@
-import fastify from "fastify";
-import cookie from "@fastify/cookie";
+import { app } from "./app";
 import { env } from "./env";
-import { transactionsRoutes } from "./routes/transactions";
-
-const app = fastify();
-
-app.register(cookie);
-
-// criando as rotas de transactions com o prefix /transactions antes de tudo ex: /transactions/create
-app.register(transactionsRoutes, {
-  prefix: "transactions",
-});
 
 // colocando porta para rodar o server
 app
